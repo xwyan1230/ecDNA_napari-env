5a749a1a40d520bad6fd75593f2dbf409514b718
Xiaowei Yan <xwyan@DERM-H4TG61HMPN7C.stanford.edu>
2022-10-22 15:59:20 -0700

    update

--- analysis/06_neuroblastoma/01_image-analysis.py
+++ analysis/06_neuroblastoma/01_image-analysis.py
@@ -10,14 +10,18 @@ import matplotlib.pyplot as plt
 import skimage.io as skio
 import shared.display as dis
 import pandas as pd
+import tifffile as tif
 import napari
 import os
 
 # INPUT PARAMETERS
 # file info
-master_folder = "/Users/xwyan/Dropbox/LAB/ChangLab/Projects/Data/20220817_neuroblastoma/"
-sample = 'NG'
+master_folder = "/Users/xwyan/Dropbox/LAB/ChangLab/Projects/Data/20221017_periphery-localization_analysis/20220826_neuroblastoma/A/"
+sample = 'V'
 save_path = master_folder
+
+# mode
+nuclear_seg = 'Y'
 
 # segmentation
 local_factor_nuclear = 151
@@ -29,9 +33,9 @@ convex_conversion_threshold = 0.85
 local_size = 100
 
 # load images
-img_nuclear = skio.imread("%s%s_2b.BMP" % (master_folder, sample))[:, :, 2]
-img_DNAFISH = skio.imread("%s%s_2g.BMP" % (master_folder, sample))[:, :, 1]
-img_centromere = skio.imread("%s%s_2r.BMP" % (master_folder, sample))[:, :, 0]
+img_nuclear = skio.imread("%s%s_b.BMP" % (master_folder, sample))[:, :, 2]
+img_DNAFISH = skio.imread("%s%s_g.BMP" % (master_folder, sample))[:, :, 1]
+img_centromere = skio.imread("%s%s_r.BMP" % (master_folder, sample))[:, :, 0]
 
 # bg_correction
 bg_val_nuclear = seg.get_bg_int([img_nuclear])[0]
@@ -45,10 +49,31 @@ img_centromere_bg_corrected = img_centromere.astype(float) - np.ones_like(img_ce
 img_centromere_bg_corrected[img_centromere_bg_corrected < 0] = 0
 
 # nuclear segmentation
-img_nuclear_seg = seg.nuclear_seg(img_nuclear_bg_corrected, local_factor=local_factor_nuclear,
-                                  min_size=min_size_nuclear, max_size=max_size_nuclear)
-img_nuclear_seg_convex = obj.label_resort(seg.obj_to_convex_filter(img_nuclear_seg,
-                                                                   threshold=convex_conversion_threshold))
+if nuclear_seg == 'Y':
+    img_nuclear_seg = seg.nuclear_seg(img_nuclear_bg_corrected, local_factor=local_factor_nuclear,
+                                      min_size=min_size_nuclear, max_size=max_size_nuclear)
+    img_nuclear_seg_convex = obj.label_resort(seg.obj_to_convex_filter(img_nuclear_seg,
+                                                                       threshold=convex_conversion_threshold))
+    img_nuclear_seg_convex_binary = np.zeros_like(img_nuclear_seg_convex)
+    img_nuclear_seg_convex_binary[img_nuclear_seg_convex > 0] = 1
+
+    # manual correction for nuclear segmentation
+    viewer = napari.Viewer()
+    viewer.add_image(img_nuclear_bg_corrected, blending='additive', colormap='blue', contrast_limits=[0, img_nuclear_bg_corrected.max()])
+    viewer.add_image(img_DNAFISH_bg_corrected, blending='additive', colormap='green',
+                     contrast_limits=[0, img_DNAFISH_bg_corrected.max()])
+    viewer.add_image(img_nuclear_seg_convex, blending='additive', colormap='viridis')
+    shapes_seg_remove = viewer.add_shapes(name='seg to be removed', ndim=2)
+    shapes_seg_add = viewer.add_shapes(name='seg to be added', ndim=2)
+    napari.run()
+
+    img_nuclear_seg_convex = ima.napari_add_or_remove(shapes_seg_remove.data, 'remove', img_nuclear_seg_convex)
+    img_nuclear_seg_convex = ima.napari_add_or_remove_obj(shapes_seg_add.data, 'add', img_nuclear_seg_convex)
+    img_nuclear_seg_convex = obj.label_resort(img_nuclear_seg_convex)
+
+else:
+    img_nuclear_seg_convex = skio.imread("%s%s_seg.tif" % (master_folder, sample), plugin="tifffile")
+
 nuclear_props = regionprops(img_nuclear_seg_convex, img_nuclear_bg_corrected)
 
 img_DNAFISH_seg = np.zeros_like(img_DNAFISH_bg_corrected)
@@ -61,6 +86,8 @@ for i in range(len(nuclear_props)):
     local_nuclear = local_nuclear[position[0]:position[1], position[2]:position[3]]
     local_DNAFISH = img_DNAFISH_bg_corrected.copy()
     local_DNAFISH = local_DNAFISH[position[0]:position[1], position[2]:position[3]]
+    local_nuclear_props = regionprops(label(local_nuclear_seg_convex))
+    local_centroid = local_nuclear_props[0].centroid
 
     # ecDNA segmentation
     local_DNAFISH_singlet = local_DNAFISH.copy()
@@ -112,15 +139,33 @@ for i in range(len(nuclear_props)):
     FISH_seg[FISH_seg_local == 1] = 1
     FISH_seg[local_nuclear_seg_convex == 0] = 0
 
-    img_DNAFISH_seg = ima.image_paste_to(img_DNAFISH_seg, FISH_seg, [int(original_centroid_nuclear[0]-100), int(original_centroid_nuclear[1]-100)])
+    img_DNAFISH_seg = ima.image_paste_to(img_DNAFISH_seg, FISH_seg,
+                                         [int(original_centroid_nuclear[0] - local_centroid[0]),
+                                          int(original_centroid_nuclear[1] - local_centroid[1])])
 
-    """viewer = napari.Viewer()
-    viewer.add_image(local_nuclear, blending='additive', colormap='blue')
-    viewer.add_image(local_DNAFISH, blending='additive', colormap='green',
-                     contrast_limits=[0, img_DNAFISH_bg_corrected.max()])
-    viewer.add_image(FISH_seg_local, blending='additive')
-    viewer.add_image(FISH_seg, blending='additive')
-    napari.run()"""
+# manual correction for ecDNA segmentation
+viewer = napari.Viewer()
+viewer.add_image(img_DNAFISH_bg_corrected, blending='additive', colormap='green',
+                 contrast_limits=[0, img_DNAFISH_bg_corrected.max()])
+viewer.add_image(img_nuclear_seg_convex, blending='additive', colormap='blue', contrast_limits=[0, 1])
+viewer.add_image(img_DNAFISH_seg, blending='additive', contrast_limits=[0, 1])
+shapes_seg_remove = viewer.add_shapes(name='seg to be removed', ndim=2)
+shapes_seg_add = viewer.add_shapes(name='seg to be added', ndim=2)
+napari.run()
+
+img_DNAFISH_seg = ima.napari_add_or_remove(shapes_seg_remove.data, 'remove', img_DNAFISH_seg)
+img_DNAFISH_seg = ima.napari_add_or_remove(shapes_seg_add.data, 'add', img_DNAFISH_seg)
+
+"""viewer = napari.Viewer()
+viewer.add_image(local_nuclear, blending='additive', colormap='blue')
+viewer.add_image(local_DNAFISH, blending='additive', colormap='green',
+                 contrast_limits=[0, img_DNAFISH_bg_corrected.max()])
+viewer.add_image(FISH_seg_local, blending='additive')
+viewer.add_image(FISH_seg, blending='additive')
+napari.run()"""
+
+tif.imwrite("%s/%s_seg.tif" % (master_folder, sample), img_nuclear_seg_convex)
+tif.imwrite("%s/%s_ecSeg.tif" % (master_folder, sample), img_DNAFISH_seg)
 
 # viewer
 viewer = napari.Viewer()
