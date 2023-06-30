02e959b6379e01bf94ef90d7a316a59510bcb66f
Xiaowei Yan <xwyan@DERM-H4TG61HMPN7C.stanford.edu>
2023-06-30 13:50:50 -0700

    update

---
+++ analysis/44_20230615_dv_check/01_dv_img-check.py
@@ -0,0 +1,9 @@
+from mrc import DVFile, imread
+import numpy as np
+
+# INPUT PARAMETERS
+# file info
+master_folder = "/Users/xwyan/Dropbox/LAB/ChangLab/Projects/Data/20230615_analysis_Ivy_images/"
+sub_folder = '20210903_CHP212_AurB_NMYC'
+
+my_array = imread('some_file.dv')
